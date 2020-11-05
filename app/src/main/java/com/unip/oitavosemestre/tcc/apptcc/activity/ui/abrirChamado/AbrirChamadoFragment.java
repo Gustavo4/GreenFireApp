2cdcae17478adf8a3a0e7ca9adf65f1c41d7e039
Gustavo Teles <gustavo.teles@gicbrasil.com>
2020-11-05 01:32:23 -0300

    Layout Abrir Chamado e alterando bg dos botões

--- app/src/main/java/com/unip/oitavosemestre/tcc/apptcc/activity/ui/abrirChamado/AbrirChamadoFragment.java
+++ app/src/main/java/com/unip/oitavosemestre/tcc/apptcc/activity/ui/abrirChamado/AbrirChamadoFragment.java
@@ -1,6 +1,7 @@
 package com.unip.oitavosemestre.tcc.apptcc.activity.ui.abrirChamado;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.unip.oitavosemestre.tcc.apptcc.R;
 
@@ -16,17 +18,33 @@ import com.unip.oitavosemestre.tcc.apptcc.R;
  */
 public class AbrirChamadoFragment extends Fragment {
 
+    private Button buttonImagem;
 
     public AbrirChamadoFragment() {
-        // Required empty public constructor
+        
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        
+        buttonImagem = container.findViewById(R.id.bt_Image_Incendio);
+        
+        buttonImagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+
         return inflater.inflate(R.layout.fragment_abrir_chamado, container, false);
+
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
     }
 
 }
